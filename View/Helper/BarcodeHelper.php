7e4f62735f518fa1238b972eba7acba2b632d059
Víctor San Martín <vsanmartinm@gmail.com>
2015-06-02 21:40:35 -0300

    clean code

--- View/Helper/BarcodeHelper.php
+++ View/Helper/BarcodeHelper.php
@@ -7,11 +7,7 @@ class BarcodeHelper extends AppHelper {
 	public $helpers = array('Html');
 
 	public function display($code, $options = array(), $attr = array()) {
-
-		$p_textUnenc = $code;
-		$p_text = rawurlencode($p_textUnenc);
-
-		$_defaults = array(
+		$defaults = array(
 			'p_bcType'           	=> 2,
 			'p_xDim'        		=> 1.5,
 			'p_charHeight'     		=> 67,
@@ -23,12 +19,14 @@ class BarcodeHelper extends AppHelper {
 			'p_rotAngle'           	=> 0,
 			'p_charGap'        		=> 1.5
 		);
-		$options = array_merge($_defaults, $options);
+		$options = array_merge($defaults, $options);
 
-
-		$dest = array('controller' => 'barcode', 'plugin' => 'barcode', 'action' => 'wrapper',
+		$dest = array(
+			'controller' => 'barcode', 
+			'plugin' => 'barcode', 
+			'action' => 'wrapper',
 			'p_bcType' => $options['p_bcType'],
-			'p_text' => $p_text,
+			'p_text' => rawurlencode($code),
 			'p_xDim' => $options['p_xDim'],
 			'p_w2n' => $options['p_w2n'],
 			'p_charGap' => $options['p_charGap'],
@@ -38,10 +36,13 @@ class BarcodeHelper extends AppHelper {
 			'p_label' => $options['p_label'],
 			'p_rotAngle' => $options['p_rotAngle'],
 			'p_checkDigit' => $options['p_checkDigit']
-
-		 );	
+		 );
 		
-		$attr['alt'] = strtoupper($code);
+		$attr = array_merge(
+			array('alt' => strtoupper($code)), 
+			$attr
+		);
+
 		return $this->Html->image($dest, $attr);
 	}	
 	
